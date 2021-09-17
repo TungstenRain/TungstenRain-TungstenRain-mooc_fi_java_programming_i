6ffcff56cadf6328b7c9bf99cfa8419ea4ec2fc0
Frank Olson <frank.olson@gmail.com>
2021-09-17 15:03:10 -0500

    first commit

---
+++ part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -0,0 +1,10 @@
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+        
+        // Print "Hi " + name
+        System.out.println("Hi " + name + "!");
+    }
+}
