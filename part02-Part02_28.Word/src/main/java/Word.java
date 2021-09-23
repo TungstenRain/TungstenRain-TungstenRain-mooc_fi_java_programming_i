2ad36eecf17007698df525bda0ffa4ff766d8a42
Frank Olson <frank.olson@gmail.com>
2021-09-23 18:12:29 -0500

    first commit

---
+++ part02-Part02_28.Word/src/main/java/Word.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/**
+ * Write a method public static String word(). The method must return a 
+ * string of your choice.
+ * 
+ * @author frank
+ */
+public class Word {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+    
+    public static String word(){
+        return "Hello there.";
+    }
+
+}
