2c9097227577c15254b23cf20593be54a240f5b2
Frank Olson <frank.olson@gmail.com>
2021-09-23 18:12:47 -0500

    first commit

---
+++ part02-Part02_30.Smallest/src/main/java/Smallest.java
@@ -0,0 +1,26 @@
+/**
+ * Define a two-parameter method smallest that returns the smaller of the two 
+ * numbers passed to it as parameters.
+ * 
+ * @author frank
+ */
+public class Smallest {
+
+    public static int smallest(int number1, int number2) {
+        // Write some code here
+        // don't print anything inside this method
+        // there must be a return in the end of the method
+        if (number1 > number2) {
+            return number2;
+        } else if (number1 < number2) {
+            return number1;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int result = smallest(2, 7);
+        System.out.println("Smallest: " + result);
+    }
+}
