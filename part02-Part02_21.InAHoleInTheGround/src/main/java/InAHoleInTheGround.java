02d72fab0be8a53bd6b2f0c5e947333c5e2e6798
Frank Olson <frank.olson@gmail.com>
2021-09-22 20:11:10 -0500

    first commit

---
+++ part02-Part02_21.InAHoleInTheGround/src/main/java/InAHoleInTheGround.java
@@ -0,0 +1,17 @@
+/**
+ * Create a method called printText which prints the phrase "In a hole in the 
+ * ground there lived a method" and a newline.
+ * 
+ * @author frank
+ */
+public class InAHoleInTheGround {
+
+    public static void main(String[] args) {
+        printText();
+    }
+    
+    public static void printText() {
+        // Print the phrase
+        System.out.println("In a hole in the ground there lived a method");
+    }
+}
