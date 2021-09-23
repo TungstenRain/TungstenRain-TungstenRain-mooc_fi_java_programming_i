f3407ca1a85e18dd39eb06fa030e26c27fb8c428
Frank Olson <frank.olson@gmail.com>
2021-09-23 18:12:37 -0500

    first commit

---
+++ part02-Part02_29.Summation/src/main/java/Summation.java
@@ -0,0 +1,18 @@
+/**
+ * Expand the method sum in the exercise template so that it calculates and 
+ * returns the sum of the numbers that are given as the parameters.
+ * 
+ * @author frank
+ */
+public class Summation {
+
+    public static int sum(int num1, int num2, int num3, int num4) {
+        // sum the values
+        return (num1 + num2 + num3 + num4);
+    }
+
+    public static void main(String[] args) {
+        int result = sum(4, 3, 6, 1);
+        System.out.println("Sum: " + result);
+    }
+}
