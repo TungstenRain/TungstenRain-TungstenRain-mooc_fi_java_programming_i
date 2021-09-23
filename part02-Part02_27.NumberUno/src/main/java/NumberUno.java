2171c29cf453dd1e1b913f0357649f0013fe68f6
Frank Olson <frank.olson@gmail.com>
2021-09-23 18:12:17 -0500

    first commit

---
+++ part02-Part02_27.NumberUno/src/main/java/NumberUno.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+/**
+ * Write a method public static int numberUno() that returns the value 1.
+ * 
+ * @author frank
+ */
+public class NumberUno {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        
+    }
+    
+    public static int numberUno(){
+        return 1;
+    }
+
+}
