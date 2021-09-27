7c40e58d7ebea21f9ec0bd1d6dbb56323aa3e490
Frank Olson <frank.olson@gmail.com>
2021-09-27 04:31:05 -0500

    first commit

---
+++ part04-Part04_06.Door/src/main/java/Door.java
@@ -0,0 +1,15 @@
+/**
+ * A Door class for Part04_06
+ * @author frank
+ */
+public class Door {
+    // Constructor
+    public Door() {
+        
+    }
+    
+    // Public methods
+    public void knock() {
+        System.out.println("Who's there?");
+    }
+}
---
+++ part04-Part04_06.Door/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Instantiate Door class
+        Door alexander = new Door();
+        
+        // Call the public method
+        alexander.knock();
+        alexander.knock();
+    }
+}
