cbd39b9078e395e32df337f458582994340bd19f
Frank Olson <frank.olson@gmail.com>
2021-09-27 04:30:33 -0500

    first commit

---
+++ part04-Part04_03.DogAttributes/src/main/java/Dog.java
@@ -0,0 +1,12 @@
+/**
+ * Dog class for Part 04_03
+ * 
+ * @author frank
+ */
+public class Dog {
+    // Class variables
+    private String name;
+    private String breed;
+    private int age;
+    
+}
