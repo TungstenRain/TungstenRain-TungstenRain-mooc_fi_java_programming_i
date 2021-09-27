f7218d108cc92191900d42c69a446cabc6a60a29
Frank Olson <frank.olson@gmail.com>
2021-09-27 04:30:42 -0500

    first commit

---
+++ part04-Part04_04.Room/src/main/java/Room.java
@@ -0,0 +1,17 @@
+/**
+ * Room Class for Part04_04
+ * 
+ * @author frank
+ */
+public class Room {
+    // Class variables
+    private String code;
+    private int seats;
+    
+    // Constructor
+    public Room(String classCode, int numberOfSeats) {
+        this.code = classCode;
+        this.seats = numberOfSeats;
+    }
+    
+}
